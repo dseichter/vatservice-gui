cf7497af75ed040b45f867550a7199d2d63b03b8
Daniel Seichter <daniel.seichter@erpware.de>
2024-04-25 22:25:53 +0200

    #6 fix PEP8 findings of PR #12

--- src/helper.py
+++ src/helper.py
@@ -6,6 +6,7 @@ import logging
 VERSION = "v0.0.1"
 URL = 'https://api.github.com/repos/dseichter/vatservice-gui/releases/latest'
 
+
 # load value from json file with given key
 def load_value_from_json_file(key):
     with open('config.json', 'r') as f:
--- src/vatservice.py
+++ src/vatservice.py
@@ -25,7 +25,7 @@ class CalcFrame(gui.MainFrame):
         self.comboBoxInterface.SetValue(helper.load_value_from_json_file('interface'))
         self.comboBoxLanguage.SetValue(helper.load_value_from_json_file('language'))
         self.textCSVdelimiter.SetValue(helper.load_value_from_json_file('delimiter'))
-        
+
         # load image from file
         image = wx.Image("../images/favicon.png", wx.BITMAP_TYPE_ANY)
         image = image.Scale(32, 32, wx.IMAGE_QUALITY_HIGH)
